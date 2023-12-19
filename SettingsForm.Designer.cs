90aa2ef26ef23a045158c98a5ef4ed19f435af80
Watislav <vladob00@yandex.com>
2023-12-19 16:48:00 +0300

    Добавьте файлы проекта.

---
+++ SettingsForm.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace QwertyToMIDI
+{
+    partial class SettingsForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SettingsForm));
+            button_Add = new Button();
+            button_Remove = new Button();
+            button_Apply = new Button();
+            dataGridView_Keys = new DataGridView();
+            Column_Key = new DataGridViewTextBoxColumn();
+            Column_Key_status = new DataGridViewTextBoxColumn();
+            Column_MIDI1 = new DataGridViewTextBoxColumn();
+            Column_MIDI2 = new DataGridViewTextBoxColumn();
+            ColumnMIDI3 = new DataGridViewTextBoxColumn();
+            label_Key = new Label();
+            textBox_Key = new TextBox();
+            label_MIDI1 = new Label();
+            label_MIDI2 = new Label();
+            label_MIDI3 = new Label();
+            numericUpDown_MIDI1 = new NumericUpDown();
+            numericUpDown_MIDI2 = new NumericUpDown();
+            numericUpDown_MIDI3 = new NumericUpDown();
+            notifyIcon_MIDIController = new NotifyIcon(components);
+            contextMenuStrip_tray = new ContextMenuStrip(components);
+            toolStripMenuItem_Show = new ToolStripMenuItem();
+            toolStripMenuItem_Exit = new ToolStripMenuItem();
+            label_MIDI_Device = new Label();
+            comboBox_Device_List = new ComboBox();
+            button_Refresh_Devices = new Button();
+            comboBox_Key_status = new ComboBox();
+            ((System.ComponentModel.ISupportInitialize)dataGridView_Keys).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown_MIDI1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown_MIDI2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown_MIDI3).BeginInit();
+            contextMenuStrip_tray.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button_Add
+            // 
+            button_Add.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            button_Add.Location = new Point(12, 359);
+            button_Add.Name = "button_Add";
+            button_Add.Size = new Size(75, 23);
+            button_Add.TabIndex = 0;
+            button_Add.Text = "Add";
+            button_Add.UseVisualStyleBackColor = true;
+            button_Add.Click += button_Add_Click;
+            // 
+            // button_Remove
+            // 
+            button_Remove.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            button_Remove.Location = new Point(93, 359);
+            button_Remove.Name = "button_Remove";
+            button_Remove.Size = new Size(75, 23);
+            button_Remove.TabIndex = 2;
+            button_Remove.Text = "Remove";
+            button_Remove.UseVisualStyleBackColor = true;
+            button_Remove.Click += button_Remove_Click;
+            // 
+            // button_Apply
+            // 
+            button_Apply.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            button_Apply.Location = new Point(540, 359);
+            button_Apply.Name = "button_Apply";
+            button_Apply.Size = new Size(75, 23);
+            button_Apply.TabIndex = 4;
+            button_Apply.Text = "Apply";
+            button_Apply.UseVisualStyleBackColor = true;
+            button_Apply.Visible = false;
+            // 
+            // dataGridView_Keys
+            // 
+            dataGridView_Keys.AllowUserToAddRows = false;
+            dataGridView_Keys.AllowUserToDeleteRows = false;
+            dataGridView_Keys.AllowUserToResizeRows = false;
+            dataGridView_Keys.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dataGridView_Keys.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView_Keys.Columns.AddRange(new DataGridViewColumn[] { Column_Key, Column_Key_status, Column_MIDI1, Column_MIDI2, ColumnMIDI3 });
+            dataGridView_Keys.Location = new Point(12, 41);
+            dataGridView_Keys.Name = "dataGridView_Keys";
+            dataGridView_Keys.ReadOnly = true;
+            dataGridView_Keys.RowTemplate.Height = 25;
+            dataGridView_Keys.Size = new Size(603, 209);
+            dataGridView_Keys.TabIndex = 5;
+            // 
+            // Column_Key
+            // 
+            Column_Key.HeaderText = "Key";
+            Column_Key.Name = "Column_Key";
+            Column_Key.ReadOnly = true;
+            // 
+            // Column_Key_status
+            // 
+            Column_Key_status.HeaderText = "Key Status";
+            Column_Key_status.Name = "Column_Key_status";
+            Column_Key_status.ReadOnly = true;
+            // 
+            // Column_MIDI1
+            // 
+            Column_MIDI1.HeaderText = "MIDI Controller";
+            Column_MIDI1.Name = "Column_MIDI1";
+            Column_MIDI1.ReadOnly = true;
+            Column_MIDI1.Width = 120;
+            // 
+            // Column_MIDI2
+            // 
+            Column_MIDI2.HeaderText = "MIDI Value";
+            Column_MIDI2.Name = "Column_MIDI2";
+            Column_MIDI2.ReadOnly = true;
+            Column_MIDI2.Width = 120;
+            // 
+            // ColumnMIDI3
+            // 
+            ColumnMIDI3.HeaderText = "MIDI Channel";
+            ColumnMIDI3.Name = "ColumnMIDI3";
+            ColumnMIDI3.ReadOnly = true;
+            ColumnMIDI3.Width = 120;
+            // 
+            // label_Key
+            // 
+            label_Key.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            label_Key.AutoSize = true;
+            label_Key.Location = new Point(12, 259);
+            label_Key.Name = "label_Key";
+            label_Key.Size = new Size(26, 15);
+            label_Key.TabIndex = 6;
+            label_Key.Text = "Key";
+            // 
+            // textBox_Key
+            // 
+            textBox_Key.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            textBox_Key.Location = new Point(44, 256);
+            textBox_Key.Name = "textBox_Key";
+            textBox_Key.ReadOnly = true;
+            textBox_Key.Size = new Size(124, 23);
+            textBox_Key.TabIndex = 7;
+            textBox_Key.KeyUp += textBox_Key_KeyUp;
+            // 
+            // label_MIDI1
+            // 
+            label_MIDI1.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            label_MIDI1.AutoSize = true;
+            label_MIDI1.Location = new Point(437, 258);
+            label_MIDI1.Name = "label_MIDI1";
+            label_MIDI1.Size = new Size(88, 15);
+            label_MIDI1.TabIndex = 11;
+            label_MIDI1.Text = "MIDI Controller";
+            // 
+            // label_MIDI2
+            // 
+            label_MIDI2.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            label_MIDI2.AutoSize = true;
+            label_MIDI2.Location = new Point(437, 287);
+            label_MIDI2.Name = "label_MIDI2";
+            label_MIDI2.Size = new Size(63, 15);
+            label_MIDI2.TabIndex = 12;
+            label_MIDI2.Text = "MIDI Value";
+            // 
+            // label_MIDI3
+            // 
+            label_MIDI3.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            label_MIDI3.AutoSize = true;
+            label_MIDI3.Location = new Point(437, 316);
+            label_MIDI3.Name = "label_MIDI3";
+            label_MIDI3.Size = new Size(79, 15);
+            label_MIDI3.TabIndex = 13;
+            label_MIDI3.Text = "MIDI Channel";
+            // 
+            // numericUpDown_MIDI1
+            // 
+            numericUpDown_MIDI1.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            numericUpDown_MIDI1.Location = new Point(531, 256);
+            numericUpDown_MIDI1.Maximum = new decimal(new int[] { 127, 0, 0, 0 });
+            numericUpDown_MIDI1.Name = "numericUpDown_MIDI1";
+            numericUpDown_MIDI1.Size = new Size(84, 23);
+            numericUpDown_MIDI1.TabIndex = 14;
+            // 
+            // numericUpDown_MIDI2
+            // 
+            numericUpDown_MIDI2.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            numericUpDown_MIDI2.Location = new Point(531, 285);
+            numericUpDown_MIDI2.Maximum = new decimal(new int[] { 127, 0, 0, 0 });
+            numericUpDown_MIDI2.Name = "numericUpDown_MIDI2";
+            numericUpDown_MIDI2.Size = new Size(84, 23);
+            numericUpDown_MIDI2.TabIndex = 15;
+            // 
+            // numericUpDown_MIDI3
+            // 
+            numericUpDown_MIDI3.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            numericUpDown_MIDI3.Location = new Point(531, 314);
+            numericUpDown_MIDI3.Maximum = new decimal(new int[] { 16, 0, 0, 0 });
+            numericUpDown_MIDI3.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            numericUpDown_MIDI3.Name = "numericUpDown_MIDI3";
+            numericUpDown_MIDI3.Size = new Size(84, 23);
+            numericUpDown_MIDI3.TabIndex = 16;
+            numericUpDown_MIDI3.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            // 
+            // notifyIcon_MIDIController
+            // 
+            notifyIcon_MIDIController.ContextMenuStrip = contextMenuStrip_tray;
+            notifyIcon_MIDIController.Icon = (Icon)resources.GetObject("notifyIcon_MIDIController.Icon");
+            notifyIcon_MIDIController.Text = "MIDI Controller";
+            notifyIcon_MIDIController.Visible = true;
+            notifyIcon_MIDIController.MouseDoubleClick += notifyIcon_MIDIController_MouseDoubleClick;
+            // 
+            // contextMenuStrip_tray
+            // 
+            contextMenuStrip_tray.Items.AddRange(new ToolStripItem[] { toolStripMenuItem_Show, toolStripMenuItem_Exit });
+            contextMenuStrip_tray.Name = "contextMenuStrip_tray";
+            contextMenuStrip_tray.Size = new Size(104, 48);
+            // 
+            // toolStripMenuItem_Show
+            // 
+            toolStripMenuItem_Show.Name = "toolStripMenuItem_Show";
+            toolStripMenuItem_Show.Size = new Size(103, 22);
+            toolStripMenuItem_Show.Text = "Show";
+            toolStripMenuItem_Show.Click += toolStripMenuItem_Show_Click;
+            // 
+            // toolStripMenuItem_Exit
+            // 
+            toolStripMenuItem_Exit.Name = "toolStripMenuItem_Exit";
+            toolStripMenuItem_Exit.Size = new Size(103, 22);
+            toolStripMenuItem_Exit.Text = "Exit";
+            toolStripMenuItem_Exit.Click += toolStripMenuItem_Exit_Click;
+            // 
+            // label_MIDI_Device
+            // 
+            label_MIDI_Device.AutoSize = true;
+            label_MIDI_Device.Location = new Point(12, 15);
+            label_MIDI_Device.Name = "label_MIDI_Device";
+            label_MIDI_Device.Size = new Size(70, 15);
+            label_MIDI_Device.TabIndex = 17;
+            label_MIDI_Device.Text = "MIDI Device";
+            // 
+            // comboBox_Device_List
+            // 
+            comboBox_Device_List.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            comboBox_Device_List.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox_Device_List.FormattingEnabled = true;
+            comboBox_Device_List.Location = new Point(88, 12);
+            comboBox_Device_List.MaxDropDownItems = 18;
+            comboBox_Device_List.Name = "comboBox_Device_List";
+            comboBox_Device_List.Size = new Size(446, 23);
+            comboBox_Device_List.TabIndex = 18;
+            // 
+            // button_Refresh_Devices
+            // 
+            button_Refresh_Devices.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            button_Refresh_Devices.Location = new Point(540, 12);
+            button_Refresh_Devices.Name = "button_Refresh_Devices";
+            button_Refresh_Devices.Size = new Size(75, 23);
+            button_Refresh_Devices.TabIndex = 19;
+            button_Refresh_Devices.Text = "Refresh";
+            button_Refresh_Devices.UseVisualStyleBackColor = true;
+            button_Refresh_Devices.Click += button_Refresh_Devices_Click;
+            // 
+            // comboBox_Key_status
+            // 
+            comboBox_Key_status.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            comboBox_Key_status.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox_Key_status.FormattingEnabled = true;
+            comboBox_Key_status.Items.AddRange(new object[] { "Key Down", "Key Up" });
+            comboBox_Key_status.Location = new Point(44, 285);
+            comboBox_Key_status.MaxDropDownItems = 2;
+            comboBox_Key_status.Name = "comboBox_Key_status";
+            comboBox_Key_status.Size = new Size(124, 23);
+            comboBox_Key_status.TabIndex = 20;
+            // 
+            // SettingsForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(627, 394);
+            Controls.Add(comboBox_Key_status);
+            Controls.Add(button_Refresh_Devices);
+            Controls.Add(comboBox_Device_List);
+            Controls.Add(label_MIDI_Device);
+            Controls.Add(numericUpDown_MIDI3);
+            Controls.Add(numericUpDown_MIDI2);
+            Controls.Add(numericUpDown_MIDI1);
+            Controls.Add(label_MIDI3);
+            Controls.Add(label_MIDI2);
+            Controls.Add(label_MIDI1);
+            Controls.Add(textBox_Key);
+            Controls.Add(label_Key);
+            Controls.Add(dataGridView_Keys);
+            Controls.Add(button_Apply);
+            Controls.Add(button_Remove);
+            Controls.Add(button_Add);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "SettingsForm";
+            Text = "Qwerty To MIDI";
+            FormClosing += SettingsForm_FormClosing;
+            Load += SettingsForm_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView_Keys).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown_MIDI1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown_MIDI2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown_MIDI3).EndInit();
+            contextMenuStrip_tray.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button_Add;
+        private Button button_Remove;
+        private Button button_Apply;
+        private DataGridView dataGridView_Keys;
+        private Label label_Key;
+        private TextBox textBox_Key;
+        private Label label_MIDI1;
+        private Label label_MIDI2;
+        private Label label_MIDI3;
+        private NumericUpDown numericUpDown_MIDI1;
+        private NumericUpDown numericUpDown_MIDI2;
+        private NumericUpDown numericUpDown_MIDI3;
+        private NotifyIcon notifyIcon_MIDIController;
+        private ContextMenuStrip contextMenuStrip_tray;
+        private ToolStripMenuItem toolStripMenuItem_Show;
+        private ToolStripMenuItem toolStripMenuItem_Exit;
+        private Label label_MIDI_Device;
+        private ComboBox comboBox_Device_List;
+        private Button button_Refresh_Devices;
+        private ComboBox comboBox_Key_status;
+        private DataGridViewTextBoxColumn Column_Key;
+        private DataGridViewTextBoxColumn Column_Key_status;
+        private DataGridViewTextBoxColumn Column_MIDI1;
+        private DataGridViewTextBoxColumn Column_MIDI2;
+        private DataGridViewTextBoxColumn ColumnMIDI3;
+    }
+}
---
+++ SettingsForm.cs
@@ -0,0 +1,212 @@
+using Microsoft.VisualBasic.ApplicationServices;
+using NAudio.CoreAudioApi;
+using NAudio.Midi;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace QwertyToMIDI
+{
+    public partial class SettingsForm : Form
+    {
+        globalKeyboardHook gkh = new globalKeyboardHook();
+
+        Keys lastKey = Keys.None;
+
+        List<settings> SettingsLists = new List<settings>();
+
+        public SettingsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SettingsForm_Load(object sender, EventArgs e)
+        {
+            gkh.KeyDown += new KeyEventHandler(gkh_KeyDown);
+            gkh.KeyUp += new KeyEventHandler(gkh_KeyUp);
+
+            comboBox_Key_status.SelectedIndex = 0;
+
+            LoadFile();
+            LoadDevices();
+        }
+
+        void gkh_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (comboBox_Device_List.Text != "")
+            {
+                for (int i = 0; i < SettingsLists.Count; i++)
+                {
+                    if ((SettingsLists[i].key == e.KeyCode) && (SettingsLists[i].key_status == "Key Down"))
+                    {
+                        MidiOut midiOut = new MidiOut(comboBox_Device_List.SelectedIndex);
+                        midiOut.Send(MidiMessage.ChangeControl(SettingsLists[i].midi1, SettingsLists[i].midi2, SettingsLists[i].midi3).RawData);
+                        midiOut.Close();
+
+                        Debug.WriteLine(SettingsLists[i].key);
+                    }
+                }
+            }
+        }
+
+        void gkh_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (comboBox_Device_List.Text != "")
+            {
+                for (int i = 0; i < SettingsLists.Count; i++)
+                {
+                    if ((SettingsLists[i].key == e.KeyCode) && (SettingsLists[i].key_status == "Key Up"))
+                    {
+                        MidiOut midiOut = new MidiOut(comboBox_Device_List.SelectedIndex);
+                        midiOut.Send(MidiMessage.ChangeControl(SettingsLists[i].midi1, SettingsLists[i].midi2, SettingsLists[i].midi3).RawData);
+                        midiOut.Close();
+
+                        Debug.WriteLine(SettingsLists[i].key);
+                    }
+                }
+            }
+        }
+
+        private void button_Add_Click(object sender, EventArgs e)
+        {
+            if (textBox_Key.Text != "")
+            {
+                dataGridView_Keys.Rows.Add(textBox_Key.Text, comboBox_Key_status.Text, numericUpDown_MIDI1.Value, numericUpDown_MIDI2.Value, numericUpDown_MIDI3.Value);
+
+                gkh.HookedKeys.Add(lastKey);
+
+                SettingsLists.Add(new settings()
+                {
+                    key = lastKey,
+                    key_status = comboBox_Key_status.Text,
+                    midi1 = (int)numericUpDown_MIDI1.Value,
+                    midi2 = (int)numericUpDown_MIDI2.Value,
+                    midi3 = (int)numericUpDown_MIDI3.Value
+                });
+
+                lastKey = Keys.None;
+
+                textBox_Key.Text = "";
+                comboBox_Key_status.SelectedIndex = 0;
+                numericUpDown_MIDI1.Value = 0;
+                numericUpDown_MIDI2.Value = 0;
+                numericUpDown_MIDI3.Value = 1;
+
+                textBox_Key.Focus();
+            }
+        }
+
+        private void button_Remove_Click(object sender, EventArgs e)
+        {
+            if (dataGridView_Keys.Rows.Count > 0)
+            {
+                if (dataGridView_Keys.SelectedCells.Count > 0)
+                {
+                    SettingsLists.RemoveAt(dataGridView_Keys.SelectedCells[0].RowIndex);
+                    gkh.HookedKeys.RemoveAt(dataGridView_Keys.SelectedCells[0].RowIndex);
+                    dataGridView_Keys.Rows.RemoveAt(dataGridView_Keys.SelectedCells[0].RowIndex);
+                }
+            }
+        }
+
+        private void textBox_Key_KeyUp(object sender, KeyEventArgs e)
+        {
+            textBox_Key.Text = e.KeyCode.ToString();
+
+            lastKey = e.KeyCode;
+        }
+
+        private void SettingsForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                this.Hide();
+                e.Cancel = true;
+            }
+            SaveFile();
+        }
+
+        private void notifyIcon_MIDIController_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+        }
+
+        private void toolStripMenuItem_Show_Click(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+        private void toolStripMenuItem_Exit_Click(object sender, EventArgs e)
+        {
+            notifyIcon_MIDIController.Visible = false;
+            notifyIcon_MIDIController.Dispose();
+
+            Application.Exit();
+        }
+
+        private void button_Refresh_Devices_Click(object sender, EventArgs e)
+        {
+            LoadDevices();
+        }
+
+        private void SaveFile()
+        {
+            string output = "D:\\MIDI Controller Settings.xml";
+
+            Stream stream = File.OpenWrite(output);
+
+            XmlSerializer serialiser = new XmlSerializer(typeof(List<settings>));
+            serialiser.Serialize(stream, SettingsLists);
+            stream.Close();
+        }
+
+        private void LoadFile()
+        {
+            string path = "D:\\MIDI Controller Settings.xml";
+
+            if (File.Exists(path))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(List<settings>));
+
+                StreamReader reader = new StreamReader(path);
+                SettingsLists = serializer.Deserialize(reader) as List<settings>;
+                reader.Close();
+
+                for (int i = 0; i < SettingsLists.Count; i++)
+                {
+                    dataGridView_Keys.Rows.Add(SettingsLists[i].key, SettingsLists[i].key_status, SettingsLists[i].midi1, SettingsLists[i].midi2, SettingsLists[i].midi3);
+
+                    gkh.HookedKeys.Add(SettingsLists[i].key);
+                }
+
+                gkh.KeyDown += new KeyEventHandler(gkh_KeyDown);
+            }
+        }
+
+        private void LoadDevices()
+        {
+            comboBox_Device_List.Items.Clear();
+
+            for (int device = 0; device < MidiOut.NumberOfDevices; device++)
+            {
+                Debug.WriteLine(MidiOut.DeviceInfo(device).ProductName);
+
+                comboBox_Device_List.Items.Add(MidiOut.DeviceInfo(device).ProductName);
+            }
+        }
+    }
+
+    public class settings
+    {
+        public Keys key { get; set; }
+        public string? key_status { get; set; }
+        public int midi1 { get; set; }
+        public int midi2 { get; set; }
+        public int midi3 { get; set; }
+    }
+}
